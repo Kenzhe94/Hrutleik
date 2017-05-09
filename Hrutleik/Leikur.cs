5759ed84bfb4a66f3538b0125bb0bc5f92847b92
Kenzhe94 <bragikall@hotmail.com>
2017-05-09 10:11:56 +0000

    B:
    Bæta við takka, og laga stuff.

--- Hrutleik/Leikur.cs
+++ Hrutleik/Leikur.cs
@@ -519,7 +519,7 @@ namespace Hrutleik
 
         private void Frjosemi_Click(object sender, EventArgs e)
         {
-            //panel1.BackgroundImage = null;
+            panel1.Hide();
             tempPlayer = "SELECT * FROM hopverkefni WHERE id='" + LeikurLeikari[0] + "'";
             List<string> lines = new List<string>();
 
@@ -531,7 +531,7 @@ namespace Hrutleik
                 foreach (string lin in lines)
                 {
                     string[] lineFromList = lin.Split(':');
-                    arr[0] = lineFromList[7];
+                    arr[0] = lineFromList[8];
 
                 }
             }
@@ -549,7 +549,7 @@ namespace Hrutleik
                 foreach (string lin in lines2)
                 {
                     string[] lineFromList2 = lin.Split(':');
-                    arr[1] = lineFromList2[7];
+                    arr[1] = lineFromList2[8];
 
                 }
             }
@@ -607,7 +607,90 @@ namespace Hrutleik
 
         private void Gerd_Click(object sender, EventArgs e)
         {
+            //panel1.BackgroundImage = null;
+            tempPlayer = "SELECT * FROM hopverkefni WHERE id='" + LeikurLeikari[0] + "'";
+            List<string> lines = new List<string>();
 
+            string[] arr = new string[2];
+            try
+            {
+                lines = gagnagrunnur.LesariSQL(tempPlayer);
+
+                foreach (string lin in lines)
+                {
+                    string[] lineFromList = lin.Split(':');
+                    arr[0] = lineFromList[9];
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            tempAI = "SELECT * FROM hopverkefni WHERE id='" + LeikurTolvan[0] + "'";
+            List<string> lines2 = new List<string>();
+
+            try
+            {
+                lines2 = gagnagrunnur.LesariSQL(tempAI);
+
+                foreach (string lin in lines2)
+                {
+                    string[] lineFromList2 = lin.Split(':');
+                    arr[1] = lineFromList2[9];
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            MessageBox.Show("Spilari er með " + arr[0] + " og tölvan er með " + arr[1]);
+
+            if (Convert.ToDouble(arr[0]) > Convert.ToDouble(arr[1]))
+            {
+                MessageBox.Show("þú vinnur");
+
+                LeikurLeikari.Add(LeikurTolvan[0]);
+                LeikurTolvan.Remove(LeikurTolvan[0]);
+
+                if (LeikurGeymari.Count > 0)
+                {
+                    LeikurLeikari.AddRange(LeikurGeymari);
+                }
+
+                //listBox1.DataSource = LeikurLeikari;
+                //listBox2.DataSource = spilAI;
+
+            }
+            else if (Convert.ToDouble(arr[0]) < Convert.ToDouble(arr[1]))
+            {
+                MessageBox.Show("þú tapar");
+
+                LeikurTolvan.Add(LeikurLeikari[0]);
+                LeikurLeikari.Remove(LeikurLeikari[0]);
+                if (LeikurGeymari.Count > 0)
+                {
+                    LeikurTolvan.AddRange(LeikurGeymari);
+                }
+                //listBox1.DataSource = LeikurLeikari;
+                //listBox2.DataSource = spilAI;
+
+            }
+            else
+            {
+                MessageBox.Show("Það er jafntefli");
+
+                LeikurGeymari.Add(LeikurLeikari[0]);
+                LeikurGeymari.Add(LeikurTolvan[0]);
+                LeikurLeikari.Remove(LeikurLeikari[0]);
+                LeikurTolvan.Remove(LeikurTolvan[0]);
+
+                //listBox1.DataSource = LeikurLeikari;
+                //listBox2.DataSource = LeikurTolvan;
+            }
+            panel2.BackgroundImage = Spil.Images[LeikurLeikari[0]];
+            //panel1.BackgroundImage = Spil.Images[LeikurTolvan[0]];
         }
 
         private void EinkunLaeris_Click(object sender, EventArgs e)
